e861032b2966f323cbdc4351b1f73bbcf87ef993
Tsundzukani-Angel <st10495904@rcconnect.edu.za>
2026-03-08 10:39:37 +0200

    Added ACII logo

--- CyberSecurityChatbot/Program.cs
+++ CyberSecurityChatbot/Program.cs
@@ -24,6 +24,20 @@ internal class Program
         {
             Console.WriteLine("Error playing audio: " + ex.Message);
         }
+        //ASCII logo    
+        Console.ForegroundColor = ConsoleColor.Cyan;//sets color for the ASCII
+        Console.WriteLine("****************************************");
+        Console.WriteLine("  ______   ______     ____   _______    ");
+        Console.WriteLine(" /  ____| |  __  \\   / __ \\ |__   __| ");
+        Console.WriteLine("|  /      | |__) /  | /  \\ |   | |     ");
+        Console.WriteLine("| |       |  ---    | |  | |   | |");
+        Console.WriteLine("| |       | |  \\ \\  | |  | |   | |  ");
+        Console.WriteLine("|  \\____  | |__/  | | \\__/ |   | |     ");
+        Console.WriteLine(" \\______| |______/   \\____/    |_|     ");
+        Console.WriteLine("\n  CYBERSECURITY AWARENESS CHATBOT     ");
+        Console.WriteLine("****************************************");
+        Console.ResetColor();
+
         
     }
 }
