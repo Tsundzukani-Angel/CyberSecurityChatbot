ed5c4678657564f77e9c3525a407613af3d5a945
Tsundzukani-Angel <st10495904@rcconnect.edu.za>
2026-03-08 09:40:16 +0200

    Added WAV voice greeting using NAudio

--- CyberSecurityChatbot/Program.cs
+++ CyberSecurityChatbot/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using NAudio.Wave;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            using (var audioFile = new AudioFileReader("greeting.wav"))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+
+                // Wait until playback finishes
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error playing audio: " + ex.Message);
+        }
+        
+    }
+}
